f5a9de7faccf770605f855a1d8cf6427f71c23ad
jan <janani6@gmail.com>
2025-07-01 12:08:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const mobileMenuButton = document.getElementById('mobile-menu-button');
     const mobileMenu = document.getElementById('mobile-menu');
 
     mobileMenuButton.addEventListener('click', function() {
         mobileMenuButton.classList.toggle('active'); // Toggle active class
-        mobileMenu.classList.toggle('hidden'); // Toggle visibility of mobile menu
+        mobileMenu.classList.toggle('show'); // Toggle visibility of mobile menu
     });
 
     // Optional: Close mobile menu when a link is clicked
     const menuLinks = mobileMenu.querySelectorAll('a');
     menuLinks.forEach(link => {
         link.addEventListener('click', () => {
-            mobileMenu.classList.add('hidden'); // Hide menu on link click
+            mobileMenu.classList.remove('show'); // Hide menu on link click
             mobileMenuButton.classList.remove('active'); // Remove active class
         });
     });
